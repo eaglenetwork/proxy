756dae16ba9912184772735b0a9d46c47aec0324
EagleNetwork <153021443+eaglenetwork@users.noreply.github.com>
2024-05-18 10:22:42 -0700

    Update uv.js

--- static/uv.js
+++ static/uv.js
@@ -1,6 +1,6 @@
-importScripts('/static/uv/uv.bundle.js');
-importScripts('/static/uv/uv.config.js');
-importScripts('/static/uv/uv.sw.js');
+importScripts('/proxy/static/uv/uv.bundle.js');
+importScripts('/proxy/static/uv/uv.config.js');
+importScripts('/proxy/static/uv/uv.sw.js');
 importScripts('https://arc.io/arc-sw-core.js');
 
 const sw = new UVServiceWorker();
